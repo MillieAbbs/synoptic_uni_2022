2837e2bc38bf377bf5d474f19e46a67a40289eb6
emma <73945324+disgustatorium@users.noreply.github.com>
2022-06-15 15:25:12 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,79 +1,13 @@
-// installing dependencies 
-const server = require("http").createServer();
-const io = require("socket.io")(server, {cors: {origin: "*"}});
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from '@mui/material/CssBaseline';
 
-const new_message_event = "newChatMessage";
+import App from "./app";
 
-// on connection to app
-io.on("connection", (socket) => {
-
-	// logs that client id has connected to console 
-	console.log(`Client: id = "${socket.id}" has connected!`);
-
-	// finds assigned room number and stores as roomId
-	const { roomId } = socket.handshake.query;
-
-	// joins the assigned room
-	socket.join(roomId);
-
-	// socket listens for new messages
-	socket.on(new_message_event, (data) => {
-
-		// emits new message to room
-		io.in(roomId).emit(new_message_event, data);
-
-	});
-
-	// on app disconnection (user closes the socket)
-	socket.on("disconnect", () => {
-
-		// logs that client id has disconnected to console 
-		console.log(`Client: id = "${socket.id}" has diconnected.`);
-
-		// leaves room based on assigned room id
-		socket.leave(roomId);
-
-	});
-
-});
-
-const express = require('express')
-const app = express()
-const axios = require('axios')
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/place/lat/:lat/long/:long/keyword/:keyword', (req, res) => {
-
-    let URL =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=" +
-        req.params.lat +
-        "%2C" +
-        req.params.long +
-        "&rankby=distance&type=" +
-        req.params.keyword +
-        "&key=AIzaSyD5znsUPPrtztE1KJcBRftdK444DyJyyDg";
-    console.log(URL)
-
-    axios.get(URL)
-        .then(function (response) {
-            res.json(response.data)
-        })
-        .catch(function (error) {
-            res.send("fail:" + error)
-        })
-})
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-// listening on port 4000 
-server.listen(4000, () => {
-
-	console.log(`The server is now online <3`);
-
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <CssBaseline />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
